91117275392e370f3762786d1f7cce1520fd3cc0
Carlo A. Scarpato <cascarpato@integra-sc.it>
2022-11-19 19:00:19 +0100

    create func chache solve

--- cachematrix.R
+++ cachematrix.R
@@ -25,7 +25,16 @@ makeCacheMatrix <- function(x = matrix()) {
          getInverse = getInverse)
 }
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+    m <- x$getInverse() # This fetches the cached value for the inverse
+    if(!is.null(m)) { # If the cache was not empty, we can just return it
+        message("getting cached data")
+        return(m)
+    }
+    # The cache was empty. We need to calculate it, cache it, and then return it.
+    data <- x$get()  # Get value of matrix
+    m <- solve(data) # Calculate inverse
+    x$setInverse(m)  # Cache the result
+    m                # Return the inverse
 }
+
