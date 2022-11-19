6cfae45981643dfa2cae9069a48b88114c37eaf1
Carlo A. Scarpato <cascarpato@integra-sc.it>
2022-11-19 18:59:37 +0100

    create func

--- cachematrix.R
+++ cachematrix.R
@@ -4,10 +4,26 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    # Define function to set the value of the matrix. It also clears the old
+    # inverse from the cache
+    set <- function(y) {
+        x <<- y    # Set the value
+        m <<- NULL # Clear the cache
+    }
+    # Define function to get the value of the matrix
+    get <- function() x
+    # Define function to set the inverse. This is only used by getinverse() when
+    # there is no cached inverse
+    setInverse <- function(inverse) m <<- inverse
+    # Define function to get the inverse
+    getInverse <- function() m
+    
+    # Return a list with the above four functions
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
-
-
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
